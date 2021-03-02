1ec74c4a7a7dc1f663f28e297ab12bd846d3dacc
Samuel Wilson <samwil56@gmail.com>
2021-03-02 19:13:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function keepGoing(array, changeValue, skipValue) {
 
  function findBy (array, findFn) {
    var val = 0
-   var numb = parse(findFn)
+   var numb = type(findFn)
    for (var i=0; i<array.length;i++) {
      if(array[i]==numb) {
        val = array[i]
