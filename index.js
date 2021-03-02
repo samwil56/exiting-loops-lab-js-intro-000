4915d30a2edfd6a45620901e494db38caf6cbc69
Samuel Wilson <samwil56@gmail.com>
2021-03-02 15:44:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function breakOut(array, changeValue, stopValue) {
   for(i=0;i<array.length;i++) {
-    if(array['i'] == stopValue) {break}
-    array['i']=changeValue['i']
+    if(array[i] == stopValue) {break}
+    else{changeValue.push(array[i])}
   }
   return changeValue
 }
