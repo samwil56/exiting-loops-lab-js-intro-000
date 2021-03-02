1e6ed9e5b6b8d8e6fc90d191df8bc8426b78ede7
Samuel Wilson <samwil56@gmail.com>
2021-03-02 15:52:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function breakOut(array, changeValue, stopValue) {
 }
 
 function keepGoing(array, changeValue, skipValue) {
-  for(var i=0; i<array.lenght; i++) {
+  for(var i=0; i<array.length; i++) {
     if(array[i]==skipValue) {continue}
     else {array[i]=changeValue}
   }
